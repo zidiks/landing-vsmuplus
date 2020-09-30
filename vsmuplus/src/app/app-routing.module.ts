e05d6adb8938568dee80918d2f8ea7661df5cdbf
Mironov Vladimir <zidiks228@gmail.com>
2020-09-30 20:22:31 +0300

    upd: dij fb deploy

--- vsmuplus/src/app/app-routing.module.ts
+++ vsmuplus/src/app/app-routing.module.ts
@@ -3,9 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from "./landing/landing.component";
 
 const routes: Routes = [
-  {
-    path: '', component: LandingComponent
-  }
+  { path: '', component: LandingComponent }, 
 ];
 
 @NgModule({
