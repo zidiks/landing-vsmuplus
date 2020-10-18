f63c86bc48f0eadcf25ca7b96049aeb1cf8b2317
Mironov Vladimir <zidiks228@gmail.com>
2020-10-18 22:02:21 +0300

    feat: add responsive

--- vsmuplus/src/app/index/index.module.ts
+++ vsmuplus/src/app/index/index.module.ts
@@ -3,6 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { IndexRoutingModule } from './index-routing.module';
 import { IndexComponent } from './index.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from "@angular/material/menu";
 
 
 
@@ -12,7 +19,14 @@ import { IndexComponent } from './index.component';
   declarations: [IndexComponent],
   imports: [
     CommonModule,
-    IndexRoutingModule
+    IndexRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule
   ]
 })
 export class IndexModule { }
